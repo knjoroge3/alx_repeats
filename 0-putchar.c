d7bc44557705e43a47d4c61673f0e2412807be64
knjoroge3 <njorogeken57@gmail.com>
2025-05-24 16:30:11 +0300

    0. _putchar

---
+++ 0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Void
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putcahr('p');
+	_putcahr('u');
+	_putcahr('t');
+	_putcahr('c');
+	_putcahr('h');
+	_putcahr('a');
+	_putcahr('r');
+	_putcahr('\n');
+}
