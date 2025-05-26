a31317a29dab3a8d42f96cdfd64a456c546220c4
knjoroge3 <njorogeken57@gmail.com>
2025-05-26 22:27:54 +0300

    2. This report, by its very length, defends itself against the risk of being read

---
+++ pointers/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the number of characters in a string
+ *
+ * @s: pointer to the character pointer *str in main
+ *
+ * Return: Total number of characters
+ */
+
+int _strlen(char *s)
+{
+	int i = *s;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+
+	return (i);
+}
