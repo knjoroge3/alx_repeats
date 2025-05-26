c0889e3f91af511381dbda5c0e957ee164fb3e7d
knjoroge3 <njorogeken57@gmail.com>
2025-05-26 20:59:45 +0300

    0. 98 Battery st.

---
+++ pointers/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pinter and calls by refernce to reset it
+ *
+ * @n: Pointer to the integer
+ *
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
